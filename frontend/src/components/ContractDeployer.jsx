f09c6d14a9131cc4bee79e7d3c2b964f0df7513a
tieubochet <tao.nguyen712@gmail.com>
2025-09-26 21:12:10 +0700

    update css

--- frontend/src/components/ContractDeployer.jsx
+++ frontend/src/components/ContractDeployer.jsx
@@ -166,7 +166,7 @@ export default function ContractDeployer() {
       />
 
       {/* Grid contracts */}
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-8">
+      <div className="grid grid-cols-3 sm:grid-cols-3 lg:grid-cols-3 gap-8">
         {contractsList.map((c, i) => (
           <ContractCard
             key={i}
